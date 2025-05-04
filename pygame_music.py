00589aa30ec7974114eff342ca33895ed0f01e0f
CassisOR <142651290+CassisOR@users.noreply.github.com>
2025-05-04 18:55:23 +0900

    Delete pygame_music.py

--- pygame_music.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('school_bell.mp3')
-pygame.mixer.music.play()
-
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
